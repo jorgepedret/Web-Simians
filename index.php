d7e8c7b95fec2cdfe8cf17e8655e5e6c6e0c2d45
jorgepedret <jorgepedret@gmail.com>
2010-11-04 02:18:58 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title>Web Simians</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico">
+	<link rel="apple-touch-icon" href="<?php bloginfo('stylesheet_directory'); ?>/apple-touch-icon.png">
+	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/style.css?v=2">
+	<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/modernizr-1.6.min.js"></script>
+</head>
+<body>
+<div id="wrap">
+<header id="header">
+	<div class="wrap">
+		<nav id="global-nav">
+			<hgroup id="branding">
+				<a href="#">
+					<h1>Web Simians</h1>
+					<h2>Web Design &amp; Development</h2>
+				</a>
+			</hgroup>
+			<ul>
+				<li class="home-item"><a href="#">Home</a></li>
+				<li class="services-item"><a href="#">Services</a></li>
+				<li class="portfolio-item"><a href="#">Portfolio</a></li>
+				<li class="contact-item"><a href="#">Contact Us</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+<section id="post-header">
+	<ul id="slide-show">
+		<li>
+			<div class="wrap">
+				<div class="question">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/img/hmmm2.png" alt="Monkey handstand talking on phone" height="240" />
+					<h3><span>What is your website doing for your business?</span></h3>
+				</div>
+				<div class="answer">
+					<p>We design keeping your business objectives in mind</p>
+					<a href="#" class="btn">Check out our portfolio &raquo;</a>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div class="wrap">
+				<div class="question">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/img/happy-sad-monkey2.png" alt="Half Bum Monkey Half Business Monkey" />
+					<h3>Does your website reflects your business identity?</h3>
+				</div>
+				<div class="answer">
+					<p>We help you understand your business potential</p>
+					<a href="#" class="btn">Check out our services &raquo;</a>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div class="wrap">
+				<div class="question">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/img/monkeys-wip.png" alt="Happy Monkey" />
+					<h3>Do you know how your website is doing?</h3>
+				</div>
+				<div class="answer">
+					<p>We get into your user&rsquo;s head <br /><small>(Like we&rsquo;re getting in yours now)</small></p>
+					<a href="#" class="btn">Find out how &raquo;</a>
+				</div>
+			</div>
+		</li>
+	</ul>
+	<div class="wrap" id="slide-nav-wrap">
+		<nav id="slide-nav">
+			<a href="#" class="prev">Previus</a>
+			<a href="#" class="next">Next</a>
+		</nav>
+	</div>
+</section>
+<article id="main">
+	<div class="wrap">
+		<section id="featured-projects">
+			<h2>Featured Projects</h2>
+			<article class="project">
+				<h3><a href="#">Vehicle Direct</a></h3>
+				<p>Toronto vehicles sale and leasing services</p>
+				<p><a href="#">http://vehicle-direct.com/</a></p>
+				<figure>
+					<a href="#">
+						<img src="<?php bloginfo('stylesheet_directory'); ?>/img/vehicle-direct.jpg" alt="" />
+					</a>
+				</figure>
+			</article>
+			<article class="project">
+				<h3><a href="#">Melissa Denuzzo</a></h3>
+				<p>Personal &amp; Traveling Blog</p>
+				<p><a href="#">http://melissadenuzzo.com/</a></p>
+				<figure>
+					<a href="#">
+						<img src="<?php bloginfo('stylesheet_directory'); ?>/img/melissa-denuzzo.jpg" alt="" />
+					</a>
+				</figure>
+			</article>
+		</section>
+		<section id="blog-entries">
+			<h2>What we're talking about</h2>
+			<article class="post">
+				<header>
+					<h3><a href="#">Objective Oriented Website: The Scientific Method</a></h3>
+				</header>
+				<p>Our youthful duty free sale appreciate, wealth roasted, spend jiffy space a is. Roasted luscious feel thin delicious deluxe listen need relaxing find.</p>
+				<footer>
+					<a href="#">Continue Reading &raquo;</a>
+				</footer>
+			</article>
+			<article class="post">
+				<header>
+					<h3><a href="#">Objective Oriented Website: The Scientific Method</a></h3>
+				</header>
+				<p>Our youthful duty free sale appreciate, wealth roasted, spend jiffy space a is. Roasted luscious feel thin delicious deluxe listen need relaxing find.</p>
+				<footer>
+					<a href="#">Continue Reading &raquo;</a>
+				</footer>
+			</article>
+		</section>
+		<article id="tweet">
+			<p>Our youthful duty free sale appreciate, wealth roasted, spend jiffy space a is. Roasted luscious feel thin delicious deluxe listen need relaxing find.</p>
+			<time>Posted 2 hours ago</time>
+		</article>
+	</div>
+</article>
+<footer id="footer">
+	<div class="wrap">
+		<article id="about">
+			<header><h3>About Web Simians</h3></header>
+			<p>We are a design &amp; development agency with a focus on improving users experience.</p>
+			<p>We are a small team of developers &amp; designers based in Vancouver, Canada, but we welcome projects of any size and from any country.</p>
+			<p>We are a young business but our team has been working on web design &amp; programming for over 10 years.</p>
+			<p>If you&rsquo;re looking for an objective oriented website, <a href="#">we&rsquo;d love to help you!</a></p>
+			
+		</article>
+		<article id="contact">
+			<header><h3>Get in Touch</h3></header>
+			<form action="#" method="post">
+				<p>We'd like to hear from you!</p>
+				<div class="for-text">
+					<label>Name:</label>
+					<input type="text" />
+				</div>
+				<div class="for-text">
+					<label>Email:</label>
+					<input type="email" />
+				</div>
+				<div class="for-textarea">
+					<label>How can we help?</label>
+					<textarea></textarea>
+				</div>
+				<div class="for-submit">
+					<input type="submit" value="Send" />
+				</div>
+			</form>
+		</article>
+		<section id="contact-info">
+			<header><h3>Contact Information</h3></header>
+			<address class="vcard">
+				Phone: <span class="phone">+1.647.502.3220</span><br />
+				Email: <span class="email">info@websimians.com</span><br />
+				<span>
+				4224 West 15th Av.<br />
+				Vancouver, B.C.<br />
+				Canada V6R 3A6
+				</span>
+			</address>
+		</section>
+	</div>
+</footer>
+</div> <!-- end of #wrap -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
+
+<!-- scripts concatenated and minified via ant build script-->
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/plugins.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js"></script>
+<!-- end concatenated and minified scripts-->
+
+<!--[if lt IE 7 ]>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/dd_belatedpng.js"></script>
+<script> DD_belatedPNG.fix('img, .png_bg'); </script>
+<![endif]-->
+
+<!-- yui profiler and profileviewer - remove for production -->
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/profiling/yahoo-profiling.min.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/profiling/config.js"></script>
+<!-- end profiling code -->
+
+<!-- change the UA-XXXXX-X to be your site's ID -->
+<script>
+var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+(function(d, t) {
+var g = d.createElement(t),
+s = d.getElementsByTagName(t)[0];
+g.async = true;
+g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+s.parentNode.insertBefore(g, s);
+})(document, 'script');
+</script>
+</body>
+</html>
